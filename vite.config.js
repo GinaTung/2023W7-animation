c7284a8b06b01276a5b7c77a3a412b38b878b39d
yuling <gb2013429@gmail.com>
2023-09-02 09:36:02 +0800

    更改repo名稱

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2023W6-orderticket/',
+  base: '/2023W7-animation/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
